72416a8a67a6de83d19a16fd800ba37b3bdf1c93
김민경 <kimminkyung@gimmingyeong-ui-MacBookAir.local>
2023-11-12 01:02:03 +0900

    Feat: 메인 페이지 구상

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,5 +1,23 @@
-export default function Home() {
+import chick from 'images/ch01_yellow.jpg'
+
+
+export default function abc() {
+
+  //const chick = 'client/public/images/ch01_yellow.jpg'
+
   return (
-    <div></div>
+    <div className="App">
+      <div className="top-box">
+        <div className="text-box">
+          <h1>TitleTitle</h1>
+          
+        </div>
+      </div>
+      <div className="centered-box">
+        <img src={chick} alt="chick" style={{ width: '50px', height: 'auto'}}/>
+      </div>
+    </div>
   )
 }
+
+

c6a889a915d227f6927fdfb77657f4b3055c5ef0
mxinseo <jade201913@gmail.com>
2023-11-12 02:42:10 +0900

    fix: Home 디자인 수정

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,19 +1,11 @@
-<<<<<<< HEAD
-// import chick from "/images/ch01_yellow.jpg";
-
 export default function abc() {
-=======
+  const chick = "client/public/images/ch01_yellow.jpg";
 
-export default function abc() {
-
-  const chick = 'client/public/images/ch01_yellow.jpg'
-
->>>>>>> f127741 (feat)
   return (
     <div className="App">
       <div className="top-box">
         <div className="text-box">
-          <h1>TitleTitle</h1>
+          <h1># 한파주의보</h1>
         </div>
       </div>
       <div className="centered-box">

0b01df43439b0881ce8dc167e2c231ccb4544262
Seonghyeon Cho <st42597@gmail.com>
2023-11-12 06:21:32 +0900

    feat: add main page api

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,5 +1,15 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
 export default function abc() {
   const chick = "client/public/images/ch01_yellow.jpg";
+
+  useEffect(() => {
+    async function getData() {
+      const data = await axios.get('/api/keyword');
+      console.log(data.data);
+    }
+  }, []);
 
   return (
     <div className="App">

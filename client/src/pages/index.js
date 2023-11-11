224d3711220f84b3cc69ddc645c058bc3503560c
김민경 <kimminkyung@gimmingyeong-ui-MacBookAir.local>
2023-11-12 01:18:47 +0900

    feat

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,6 +1,14 @@
+<<<<<<< HEAD
 // import chick from "/images/ch01_yellow.jpg";
 
 export default function abc() {
+=======
+
+export default function abc() {
+
+  const chick = 'client/public/images/ch01_yellow.jpg'
+
+>>>>>>> f127741 (feat)
   return (
     <div className="App">
       <div className="top-box">

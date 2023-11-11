c7b6929b0719e70046d49976c0be6a52e032f3af
mxinseo <jade201913@gmail.com>
2023-11-12 01:14:41 +0900

    Feat: 메인 페이지 캐릭터 이미지 스타일 변경

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,23 +1,16 @@
-import chick from 'images/ch01_yellow.jpg'
-
+// import chick from "/images/ch01_yellow.jpg";
 
 export default function abc() {
-
-  //const chick = 'client/public/images/ch01_yellow.jpg'
-
   return (
     <div className="App">
       <div className="top-box">
         <div className="text-box">
           <h1>TitleTitle</h1>
-          
         </div>
       </div>
       <div className="centered-box">
-        <img src={chick} alt="chick" style={{ width: '50px', height: 'auto'}}/>
+        <div className="chick-img" />
       </div>
     </div>
-  )
+  );
 }
-
-

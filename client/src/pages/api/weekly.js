e3cd3286ecec35a4639b2085ed5c88b9ac8cc4d2
Seonghyeon Cho <st42597@gmail.com>
2023-11-12 03:24:33 +0900

    feat: add api get and display it

--- client/src/pages/api/weekly.js
+++ client/src/pages/api/weekly.js
@@ -4,7 +4,6 @@ export default function handler(req, res) {
   console.log(req.query)
   async function getData() {
     const data = await axios.get('http://localhost:8000/weekly', { params: req.query }).then((response) => response.data);
-    console.log(data);
     res.status(200).json(data);
   }
   getData();
--- client/src/pages/calendar/index.js
+++ client/src/pages/calendar/index.js
@@ -3,8 +3,9 @@ import axios from 'axios'
 
 export default function Calendar() {
   const korDay = ['일', '월', '화', '수', '목', '금', '토'];
-  const [selectedDay, setSelectedDay] = useState('');
-  const [task, setTask] = useState(['task 1', 'task 2', 'task 3']);
+  const [selectedDay, setSelectedDay] = useState(0);
+  const [task, setTask] = useState([]);
+  const [selectedTask, setSelectedTask] = useState([]);
 
   useEffect(() => {
     async function getData() {
@@ -14,46 +15,65 @@ export default function Calendar() {
           username: 'asdf'
         }
       });
-      console.log(data);
+      console.log(data.data)
+      setTask(data.data);
     }
     getData();
-  }, [])
+  }, []);
+
+  useEffect(() => {
+    if(task.length === 0) return;
+    setSelectedTask(task[selectedDay].data);
+  }, [selectedDay]);
 
 
 
   return (
-    <div className="">
+    <div className="mb-20">
       <div className="carousel w-full flex justify-between p-2 my-10">
         {
           korDay.map((x, i) => 
-            <div
-              className="carousel-item px-3 py-2 border rounded-box" key={i}
-              onClick={() => setSelectedDay(x)}
-            >{x}</div>
+            <div className="indicator">
+              {
+                task.length > 0 &&
+                task[i].data.length > 0 &&
+                <span className="indicator-item badge badge-secondary"></span>
+              }
+              <div
+                className="carousel-item px-3 py-2 border rounded-box" key={i}
+                onClick={() => setSelectedDay(i)}
+              >{x}</div>
+            </div>
           )
         }
       </div>
       
-      <h1 className="text-2xl mb-6">Today's Task</h1>
+      <h1 className="text-2xl mb-6">{task[selectedDay] && task[selectedDay].day} Task</h1>
       <div className="rounded-box border p-4">
-        <h2 className="ml-4 mb-4 text-xl">{task.length} tasks</h2>
+        <h2 className="ml-4 mb-4 text-xl">{selectedTask.length} tasks</h2>
         {
-          task.map((x, i) => 
+          selectedTask.map((x, i) => 
             <div className="flex items-center p-2" key={i}>
-              <input type="checkbox" className="checkbox mr-4" />
-              <input type="text" value={x} onChange={(y) => {
-                let _task = [...task];
+              <input type="checkbox" checked={x.checked ? 'checked' : ''} className="checkbox mr-4"
+                onClick={() => {
+                  let _task = [...selectedTask];
+                  _task[i].checked = !_task[i].checked;
+                  setSelectedTask(_task);
+                }}
+              />
+              <input type="text" value={x.task} onChange={(y) => {
+                let _task = [...selectedTask];
                 _task[i] = y.target.value;
-                setTask(_task);
+                setSelectedTask(_task);
               }} placeholder="Type here" className="input input-ghost input-sm w-full mr-4" />
               <svg
                 onClick={() => {
                   let _task = [];
-                  for(let j = 0; j < task.length; j++){
+                  for(let j = 0; j < selectedTask.length; j++){
                     if(j == i) continue;
-                    _task.push(task[j]);
+                    _task.push(selectedTask[j]);
                   }
-                  setTask(_task);
+                  setSelectedTask(_task);
                 }}
                xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 16 16"><g fill="currentColor"><path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/><path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/></g></svg>
             </div>
@@ -61,9 +81,9 @@ export default function Calendar() {
         }
         <span className="text-2xl w-full flex justify-center"
           onClick={() => {
-            let _task = [...task];
+            let _task = [...selectedTask];
             _task.push('');
-            return setTask(_task);
+            return setSelectedTask(_task);
           }}
         >+</span>
         

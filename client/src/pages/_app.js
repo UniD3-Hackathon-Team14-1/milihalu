6e00d7109345222964bbdd40aa93255c96b89abd
Seonghyeon Cho <st42597@gmail.com>
2023-11-11 20:40:54 +0900

    feat: add bottom navigation

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -1,5 +1,11 @@
 import '@/styles/globals.css'
+import Navigator from '@/components/navigator'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Navigator />
+    </>
+  );
 }

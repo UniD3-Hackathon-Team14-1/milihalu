85f0cd0e34f402728a18e5ba2fd3cbe5378b2d0d
Seonghyeon Cho <st42597@gmail.com>
2023-11-12 03:50:45 +0900

    feat: add put daily api

---
+++ client/src/pages/api/updateSchedule.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+export default function handler(req, res) {
+  console.log(req);
+  async function putData() {
+    const data = await axios.put('http://localhost:8000/daily', req.body).then((response) => response.data);
+    res.status(200).json(data);
+  }
+  putData();
+}
--- client/src/pages/calendar/index.js
+++ client/src/pages/calendar/index.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 export default function Calendar() {
   const korDay = ['일', '월', '화', '수', '목', '금', '토'];
-  const [selectedDay, setSelectedDay] = useState(0);
+  const [selectedDay, setSelectedDay] = useState(-1);
   const [task, setTask] = useState([]);
   const [selectedTask, setSelectedTask] = useState([]);
 
@@ -26,11 +26,23 @@ export default function Calendar() {
     setSelectedTask(task[selectedDay].data);
   }, [selectedDay]);
 
+  useEffect(() => {
+    if(task.length === 0) return;
+    let _task = [...task];
+    _task[selectedDay].data = selectedTask;
+    setTask(_task);
+  }, [selectedTask]);
 
+  function updateScheduleHandler(body) {
+    async function updateData() {
+      const response = await axios.put('/api/updateSchedule', body);
+    }
+    updateData();
+  }
 
   return (
     <div className="mb-20">
-      <div className="carousel w-full flex justify-between p-2 my-10">
+      <div className="carousel w-full flex justify-between p-2 py-4 my-6">
         {
           korDay.map((x, i) => 
             <div className="indicator">
@@ -55,15 +67,26 @@ export default function Calendar() {
           selectedTask.map((x, i) => 
             <div className="flex items-center p-2" key={i}>
               <input type="checkbox" checked={x.checked ? 'checked' : ''} className="checkbox mr-4"
-                onClick={() => {
+                onChange={() => {
                   let _task = [...selectedTask];
                   _task[i].checked = !_task[i].checked;
+                  updateScheduleHandler({
+                    "username": "asdf",
+                    "day": task[selectedDay].day,
+                    "data": _task
+                  });
                   setSelectedTask(_task);
                 }}
               />
               <input type="text" value={x.task} onChange={(y) => {
                 let _task = [...selectedTask];
-                _task[i] = y.target.value;
+                _task[i].task = y.target.value;
+                updateScheduleHandler({
+                  "username": "asdf",
+                  "day": task[selectedDay].day,
+                  "data": _task
+                });
+                console.log(_task)
                 setSelectedTask(_task);
               }} placeholder="Type here" className="input input-ghost input-sm w-full mr-4" />
               <svg
@@ -73,6 +96,11 @@ export default function Calendar() {
                     if(j == i) continue;
                     _task.push(selectedTask[j]);
                   }
+                  updateScheduleHandler({
+                    "username": "asdf",
+                    "day": task[selectedDay].day,
+                    "data": _task
+                  });
                   setSelectedTask(_task);
                 }}
                xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 16 16"><g fill="currentColor"><path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/><path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/></g></svg>
@@ -82,7 +110,12 @@ export default function Calendar() {
         <span className="text-2xl w-full flex justify-center"
           onClick={() => {
             let _task = [...selectedTask];
-            _task.push('');
+            _task.push({checked: false, task: ''});
+            updateScheduleHandler({
+              "username": "asdf",
+              "day": task[selectedDay].day,
+              "data": _task
+            });
             return setSelectedTask(_task);
           }}
         >+</span>
